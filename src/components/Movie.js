e7b7f49f42f0f3983a1ad03c18cfbbaad62bed98
Phillip Choi <pchoi13370@gmail.com>
2019-01-26 00:30:03 -0800

    Added Link component from React Router to movies to change path

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Movie = ({ details }) => {
   return (
-    <div className='movie'>
-      <h1>some movie</h1>
-      <img
-        src={`https://image.tmdb.org/t/p/w300${details.poster_path}`}
-        alt=''
-      />
-    </div>
+    <Link to={`/movies/${details.id}`}>
+      <div className='movie'>
+        <h1>some movie</h1>
+        <img
+          src={`https://image.tmdb.org/t/p/w300${details.poster_path}`}
+          alt={`${details.title}`}
+        />
+      </div>
+    </Link>
   );
 };
 
