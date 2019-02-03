ee3bbb3282c3c4e9d7c0a62ef8c96ed434b3f79a
Phillip Choi <pchoi13370@gmail.com>
2019-02-03 14:04:30 -0800

    refactored fetch function into helper.js, set up Movie component with own state

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import { getMovieDetails } from "../helper";
 
 export default class Movie extends Component {
+  state = {
+    title: null,
+    overview: null
+  };
+
+  componentDidMount() {
+    console.log("mounted");
+    const movieid = this.props.location.movieid;
+    const movieData = getMovieDetails(movieid);
+    movieData.then(results => {
+      this.setState({
+        title: results.original_title,
+        overview: results.overview
+      });
+    });
+  }
+
   render() {
+    const { title, overview } = this.state;
     return (
       <div>
-        <h1>I am each movie!</h1>
+        <h1>{title}</h1>
+        <p>{overview}</p>
       </div>
     );
   }
--- src/components/MovieThumb.js
+++ src/components/MovieThumb.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const MovieThumb = ({ id, img, alt }) => {
   return (
-    <Link to={`/movies/${id}`}>
+    <Link to={{ pathname: `/movies/${id}`, movieid: id }}>
       <div className='movie'>
         <img src={`https://image.tmdb.org/t/p/w300${img}`} alt={alt} />
       </div>
