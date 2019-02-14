87484fdcd133c50057f6ee8dd7ba1068073182b7
Phillip Choi <pchoi13370@gmail.com>
2019-02-14 01:41:07 -0800

    injected global style for box sizing on the application

--- src/App.js
+++ src/App.js
@@ -4,15 +4,30 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Movie from "./components/Movie";
 import NotFound from "./components/NotFound";
+import { createGlobalStyle } from "styled-components";
 
-const App = () => {
-  return (
-    <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/movies/:movieid' component={Movie} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-};
+const GlobalStyle = createGlobalStyle`
+ html {
+   box-sizing: border-box;
+ }
+ *, *:before, *:after {
+   box-sizing: inherit;
+ }
+`;
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <GlobalStyle />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/movies/:movieid' component={Movie} />
+          <Route component={NotFound} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
+}
 
 export default App;
