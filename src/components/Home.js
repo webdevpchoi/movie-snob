88b129e30d2c36f098f55853971206b98333f93d
unknown <pchoi13370@gmail.com>
2019-03-26 23:46:17 -0700

    Added helper functions for refactoring

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "../App.css";
+import { getCast, getDetails } from "../helper";
 //components
 import Header from "./Header";
 import HeroImage from "./HeroImage";
@@ -54,6 +55,9 @@ class Home extends Component {
     movies.then(movieObj => {
       this.setState({ movies: movieObj, loading: false });
     });
+
+    getDetails(297802);
+    getCast(297802);
   };
   //get a random movie from trending and place it in HeroImage
   setRandomMovie = movieArr => {
--- src/helper.js
+++ src/helper.js
@@ -1,7 +1,20 @@
-export const isObjEmpty = obj => {
-  if (Object.entries(obj).length === 0) {
-    return true;
-  } else {
-    return false;
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+export const getCast = async movieId => {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${API_KEY}`;
+  await fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data));
+};
+
+export const getDetails = async movieId => {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(`Couldn't fetch the endpoint!`);
+    console.log(err);
   }
 };
