b8670fcb5f2e2d124a180f61173d58ee4f68da3e
unknown <pchoi13370@gmail.com>
2019-04-15 00:46:30 -0700

    added in styling for the details page

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -3,13 +3,52 @@ import styled from "styled-components/macro";
 import Header from "./Header";
 
 const StyledMovieDetails = styled.div`
-  margin: 82px 5% 0 20%;
-  padding-top: 50px;
+  padding: calc(82px + 10%) 7% 0 160px;
+  position: relative;
   .movie-info {
     background: #181f36;
-  }
-  img {
-    width: 200px;
+    min-height: 800px;
+    padding: 15px 15px 15px 20%;
+    text-align: center;
+    color: #fff;
+
+    div:first-of-type {
+      margin: 15px 0;
+    }
+    img {
+      position: absolute;
+      top: 250px;
+      left: 40px;
+      width: 300px;
+    }
+    h1 {
+      font-size: 3rem;
+      margin-bottom: 0rem;
+      letter-spacing: 1px;
+    }
+    a {
+      font-size: 1.3rem;
+      text-transform: lowercase;
+      color: #4b73ff;
+    }
+    p {
+      max-width: 700px;
+    }
+    .pie-chart {
+      background: ${props => props.theme.mainColor};
+    }
+    .genres {
+      display: flex;
+      justify-content: space-around;
+      list-style-type: none;
+
+      li {
+        padding: 10px 15px;
+        border-radius: 5px;
+        background: #1f2844;
+        font-style: italic;
+      }
+    }
   }
 `;
 
@@ -25,13 +64,15 @@ export default function MovieDetails() {
           />
           <h1>Aquaman</h1>
           <a href='#'>www.Aquaman.com</a>
-          <span>Release: January 2, 2019</span>
-          <span>Runtime: 129 mins</span>
+          <div>
+            <span>Release: January 2, 2019</span>
+            <span>Runtime: 129 mins</span>
+          </div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
-            ipsa, qui aliquam a autem eligendi maiores itaque minima fugiat
-            quibusdam, aliquid rem optio officia nulla molestias culpa! Quidem,
-            in laboriosam.
+            Overview: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Repellat, ipsa, qui aliquam a autem eligendi maiores itaque minima
+            fugiat quibusdam, aliquid rem optio officia nulla molestias culpa!
+            Quidem, in laboriosam.
           </p>
           <div className='pie-chart'>Piechart here</div>
           <ul className='genres'>
